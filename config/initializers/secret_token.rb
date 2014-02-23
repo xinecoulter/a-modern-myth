b06bc611e2b37f3c023a67c9eca578f63a76dbaa
Christine Coulter <christinetcoulter@gmail.com>
2014-02-22 21:51:14 -0500

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+AModernMyth::Application.config.secret_token = 'bcaa76d6df7bc5bdb356ce0afe222e4ea37eb1eb079a5921b4ee646c15a50c8344a183b7a4937b9566181c6021d788a07a551fe5382dcca5744b897c3bad3c9a'
