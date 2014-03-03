d31301048adbd3d2f3a5458774d0a488329d0996
Christine Coulter <christinetcoulter@gmail.com>
2014-03-03 00:17:42 -0500

    Add routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,7 @@
 AModernMyth::Application.routes.draw do
   root :to => 'welcome#index'
+  resources :band, only: [:index]
+  resources :photos, only: [:index]
+  resources :shows, only: [:index]
+  resources :contact, only: [:index]
 end
