0162a7de2dbd9ad162b5c03a1f3e55d5a18a5587
Christine Coulter <christinetcoulter@gmail.com>
2014-03-03 20:30:19 -0500

    Edit navbar partial to include dropdown for photos and videos under media. Add route for videos. Create videos controller. Create videos index view.

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,7 @@ AModernMyth::Application.routes.draw do
   root :to => 'welcome#index'
   resources :band, only: [:index]
   resources :photos, only: [:index]
+  resources :videos, only: [:index]
   resources :shows, only: [:index]
   resources :contact, only: [:index]
 end
